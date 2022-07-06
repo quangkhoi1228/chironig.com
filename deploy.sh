844bf2946341c5ef7f0f1130f83ec038a35f2cb1
Khôi <quangkhoiuit98@gmail.com>
2022-07-06 14:44:19 +0700

    add deploy server

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-ssh shinobiweb@vpsserver
-cd /home/shinobiweb/chironig.com
-python -m SimpleHTTPServer 8081 &
+ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; git pull'
+ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; sh deploy-server.sh'
