8c9144c8242bd2cba68f565c9b9b98e1cbd72361
Khôi <quangkhoiuit98@gmail.com>
2022-07-06 15:17:57 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; git pull'
-ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; sh server-deploy.sh & disown'
+ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; git pull ; nohup sh server-deploy.sh & ; exit'
