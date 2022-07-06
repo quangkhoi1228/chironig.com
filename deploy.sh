aeee947946a1f703a8d44aa8a0cd1ce6f53b1196
Khôi <quangkhoiuit98@gmail.com>
2022-07-06 15:29:49 +0700

    update css

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; git pull'
-ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ && sh server-deploy.sh'
+ssh shinobiweb@vpsserver 'cd /home/shinobiweb/chironig.com/ ; sh server-deploy.sh &'
